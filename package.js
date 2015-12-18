3588d242845a5b7a0a26bfe441432262397c1fff
Rob Fallows <rob.fallows@gmail.com>
2015-12-18 11:56:49 +0000

    add git link to package.js

--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
   name: 'tunguska:imgur',
   version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  summary: 'OAuth handler for Imgur',
+  git: 'https://github.com/robfallows/tunguska-imgur',
   documentation: 'README.md'
 });
 
